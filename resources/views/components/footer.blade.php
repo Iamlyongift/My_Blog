a3f2ccbaa53ba64523f197550a447fdd9a567992
iamlyongift <chukwumagift6036@gmail.com>
2024-11-26 08:42:12 +0100

    modified footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -17,11 +17,8 @@
                 <div class="w-full md:w-1/4">
                     <h4 class="font-semibold text-lg mb-4">HELP & INFORMATION</h4>
                     <ul class="flex gap-3 flex-col">
-                        <li><a href="#" class="text-gray-400 hover:text-white">Order Status</a></li>
-                        <li><a href="#" class="text-gray-400 hover:text-white">Careers</a></li>
                         <li><a href="#" class="text-gray-400 hover:text-white">Privacy Policy</a></li>
                         <li><a href="#" class="text-gray-400 hover:text-white">Terms of Use</a></li>
-                        <li><a href="#" class="text-gray-400 hover:text-white">Why Buy Direct</a></li>
                         <li><a href="#" class="text-gray-400 hover:text-white">Newsletter</a></li>
                     </ul>
                 </div>
@@ -31,32 +28,19 @@
                     <h4 class="font-semibold text-lg mb-4">ABOUT US</h4>
                     <ul class="flex gap-3 flex-col">
                         <li><a href="#" class="text-gray-400 hover:text-white">Help Center</a></li>
-                        <li><a href="#" class="text-gray-400 hover:text-white">Store Locations</a></li>
-                        <li><a href="#" class="text-gray-400 hover:text-white">We Deliver Almost Anywhere!</a>
-                        </li>
+
                         <li><a href="#" class="text-gray-400 hover:text-white">Registry</a></li>
                         <li><a href="#" class="text-gray-400 hover:text-white">Privacy Policy</a></li>
                         <li><a href="#" class="text-gray-400 hover:text-white">Terms of Service</a></li>
                     </ul>
                 </div>
 
-                <!-- Categories Section -->
-                <div class="w-full md:w-1/4">
-                    <h4 class="font-semibold text-lg mb-4">CATEGORIES</h4>
-                    <ul class="flex gap-3 flex-col">
-                        <li><a href="#" class="text-gray-400 hover:text-white">Court & Indoor Trainers</a></li>
-                        <li><a href="#" class="text-gray-400 hover:text-white">Backpacks & Rucksacks</a></li>
-                        <li><a href="#" class="text-gray-400 hover:text-white">American Football</a></li>
-                        <li><a href="#" class="text-gray-400 hover:text-white">Racquetball</a></li>
-                        <li><a href="#" class="text-gray-400 hover:text-white">Sports Nutrition</a></li>
-                        <li><a href="#" class="text-gray-400 hover:text-white">MMA and Martial Arts</a></li>
-                    </ul>
-                </div>
+
 
                 <!-- Newsletter Section -->
                 <div class="w-full md:w-1/4">
                     <h4 class="font-semibold text-lg mb-4">The good stuff in your inbox</h4>
-                    <p class="text-gray-400 mb-4">News & updates from Sportite. No spam, we promise.</p>
+                    <p class="text-gray-400 mb-4">News & updates from My_App. No spam, we promise.</p>
                     <form class="flex flex-col md:flex-row">
                         <input type="email" placeholder="Email"
                             class="w-full md:w-auto px-3 py-2 rounded-t md:rounded-l md:rounded-t-none bg-gray-800 text-white focus:outline-none">
@@ -70,7 +54,7 @@
             <!-- Footer Bottom -->
             <div class="flex flex-col md:flex-row md:justify-between items-center mt-10 border-t border-gray-700 pt-6">
                 <p class="text-gray-400 mb-4 md:mb-0">Website by <a href="#"
-                        class="text-yellow-500 hover:underline">LYONGIFT</a>. Copyright Sportite</p>
+                        class="text-yellow-500 hover:underline">LyonGift</a>. Copyright My_App</p>
 
                 <!-- Social Media Links -->
                 <div class="flex space-x-4 mb-4 md:mb-0">
@@ -80,13 +64,6 @@
                     <a href="#" class="text-gray-400 hover:text-white">Instagram</a>
                 </div>
 
-                <!-- Payment Methods -->
-                <div class="flex space-x-4">
-                    <img src="https://via.placeholder.com/40x25" alt="Visa">
-                    <img src="https://via.placeholder.com/40x25" alt="PayPal">
-                    <img src="https://via.placeholder.com/40x25" alt="MasterCard">
-                    <img src="https://via.placeholder.com/40x25" alt="Maestro">
-                </div>
             </div>
         </div>
     </footer>
